c4480cbd2a280e72cccad6b07f4eba85f471444e
John Bell <johnlb@umich.edu>
2015-10-30 18:47:16 -0400

    idk

--- tools/init_hotelcalifornia.m
+++ tools/init_hotelcalifornia.m
@@ -5,6 +5,9 @@ if (~exist('DATA_LOADED'))
 	[x t x_names fs] = readin_hotelcalifornia();
 	ts = 1/fs;
 
+	dn = 589;
+	x(:,5) = [zeros(dn,1); x(1:end-dn,5)];
+
 elseif(~strcmp(DATA_LOADED, 'hotel_california'))
 	error('Other data is loaded. Please do a "clear all" first.');
 end
