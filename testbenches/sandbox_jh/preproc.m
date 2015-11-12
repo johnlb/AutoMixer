69d3ab8fc367cf980e31f20cab64b601ee1dee97
Jianqing Huang <jianhuan@umich.edu>
2015-11-11 20:18:54 -0500

    power spectrum for 1/3 octave

--- testbenches/sandbox_jh/preproc.m
+++ testbenches/sandbox_jh/preproc.m
@@ -1,21 +1,3 @@
-% %---- generate a pink-noise
-% Nx = 2^16;  % number of samples to synthesize
-% B = [0.049922035 -0.095993537 0.050612699 -0.004408786];
-% A = [1 -2.494956002   2.017265875  -0.522189400];
-% nT60 = round(log(1000)/(1-max(abs(roots(A))))); % T60 est.
-% v = randn(1,Nx+nT60); % Gaussian white noise: N(0,1)
-% x = filter(B,A,v);    % Apply 1/F roll-off to PSD
-% x = x(nT60+1:end);    % Skip transient response
-% %---- adding a 1000 Hz sinusoidal signal
-% fs = 48000;
-% t = [0:Nx-1]/fs;
-% f1 = 2000;
-% a = 0.05;
-% s1 = a*sin(2*pi*f1*t);
-% s = x'+s1';
-% %---- wave file (or mp2 file) read and write
-% fName = 'test.wav';
-% audiowrite(fName,s,fs);
 fName = 'MASTER.mp3';
 [s,fs] = audioread(fName);
 %%---- calculate the magnitude of frequency components
