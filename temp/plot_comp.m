35e6a77a6b387f70453b623b4d489ca61acd5dc4
jingsss <Jing@0587395444.wireless.umich.net>
2015-12-12 21:56:32 -0500

    pic

--- temp/plot_comp.m
+++ temp/plot_comp.m
@@ -1,18 +1,21 @@
 %plot & compare
-function [] = plot_gains(time,a, a_o,fig)
+function [] = plot_gains(time,x, a, a_o,fig)
 
 	K = size(a,2);
 
 	figure(fig);
 	for ii = 1:K
 		subplot(ceil(K/2),2,ii);
+        plot(time, x(:,ii)/max(max(x))/2,'y');
+        hold on
 		plot(time, a(:,ii));
         ylabel('gain')
         hold on 
         plot(time, a_o(:,ii));
-        legend('estimated', 'real')
+        
+        legend('input', 'real','estimated')
 		title(sprintf('Track %i',ii));
-		ylim([-2 10]);
+        ylim([-2 5]);
 	end
 
 end
--- temp/vali.m
+++ temp/vali.m
@@ -3,35 +3,40 @@
 run('../always.m');
 init_hotelcalifornia();
 %}
-%{
-piece = 170000 : 220000;
-xL_p = xL(piece,4:5);
-xR_p = xR(piece,4:5);
-time = (170000: 220000)*ts;
-gainL = repmat(abs(sin(time * 4)),2,1)';
-gainR = repmat(abs(cos(time * 5)),2,1)';
+
+piece = 100000 : 4000000;
+xL_p = xL(piece,:);
+xR_p = xR(piece,:);
+time = (100000 : 4000000)*ts;
+gainL = repmat(abs(sin(time / 5)),6,1)';
+gainR = repmat(abs(cos(time / 8)),6,1)';
 tL_p = sum(gainL.*xL_p, 2);
 tR_p = sum(gainR.*xR_p, 2);
+%{
 win_len = 100:100:3000;
 rms_win = 1: 5: 100;
 mse = zeros(length(win_len), length(rms_win));
 for i = 1:length(win_len)
     for j = 1:length(rms_win)
-        [aL aR] = find_coeffs(xL_p,xR_p, tL_p,tR_p, ts, win_len(i) ,2, rms_win(j));
-        mse(i,j) = mean(sum((aL - gainL).^2)) + mean(sum((aR - gainR).^2));
+        [aL aR] = find_coeffs(xL_p,xR_p, tL_p,tR_p, ts, win_len(i) ,2, rms_win(j),'pchip');
+        mse(i,j) = mean(sum((xL_p > 0) .* ((aL - gainL).^2))) + mean(sum((xR_p > 0) .* ((aR - gainR).^2)));
     end
 end
-%}
 [i,j] = find(mse==min(mse(:)))
+
 [aL aR] = find_coeffs(xL_p,xR_p, tL_p,tR_p, ts, win_len(i(1)) ,2,  rms_win(j(1)), 'pchip');
-plot_comp(time,aL,gainL,1);
-plot_comp(time,aR,gainR,2);
+%}
+[aL aR] = find_coeffs(xL_p,xR_p, tL_p,tR_p, ts, 3000 ,2,  96, 'pchip');
+plot_comp(time,xL_p, aL,gainL,1);
+plot_comp(time,xR_p, aR,gainR,2);
+%{
 figure(3)
 surf(rms_win(10:end), win_len, mse(:,10:end));
 shading interp
 xlabel('rms window size');
 ylabel('window size');
 zlabel('mse');
+%}
 %{
 mseL = []
 mseR = []
